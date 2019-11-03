507b9f36ac282a1a5fc267890376d87e23e72d89
susmitvengurlekar <susmit.py@gmail.com>
2019-11-03 13:29:32 +0530

    dfs

--- all_paths_dfs.py
+++ all_paths_dfs.py
@@ -8,60 +8,125 @@ Created on Sat Nov  2 12:41:43 2019
 
 from time import sleep
 from collections import deque
-adj_list={1:[2,3],2:[4,5],3:[6,7],4:[],5:[],6:[],7:[]}
+# =============================================================================
+# adj_list={1:[2,3],2:[4,5],3:[6,7],4:[],5:[],6:[],7:[]}
+# 
+# stack = deque()
+# routes = []
+# 
+# def push_children(adj_list, stack, routes,curr_route,links_remaining):
+#     sleep(0.3)
+#     print("Function Called")
+#     print("Stack: ", stack)
+#     print("Routes: ", routes)
+#     print("Curr Route: ",curr_route)
+#     print("Links: ",links_remaining)
+#     
+# 
+#     
+#     
+#     if links_remaining == 0:
+#         if type(stack[-1]) == str:
+#             return routes
+#         if len(stack) == 0 or len(stack) == 1:
+#             return routes
+#         stack.pop()
+#         return push_children(adj_list,stack,routes,curr_route[:-1],1)
+#     if type(stack[-1])==str:
+#         stack.pop()
+#     curr_route.append(stack[-1])
+#  
+# 
+#     if len(adj_list[curr_route[-1]]) != 0:
+#         stack.append(str(len(adj_list[curr_route[-1]])))
+#         for next_node in adj_list[curr_route[-1]]:
+#             stack.append(next_node)
+#             
+#         print("Children Added")
+#         print("Stack: ", stack)
+#         return push_children(adj_list,stack,routes,curr_route,len(adj_list[curr_route[-1]]))
+#     
+#     print("One Path Complete")
+#     
+#     routes.append(curr_route)    
+#     curr_route = curr_route[:-1]
+#     stack.pop()
+#     
+#     print("Routes: ", routes)
+#     print("Stack: ", stack)
+#     
+#     
+#     print("In middle of a branch")
+#     return push_children(adj_list,stack,routes,curr_route,links_remaining-1)
+#     
+# 
+# for src in adj_list.keys():
+#     stack.append(src)
+#     routes = push_children(adj_list,stack,routes,[],len(adj_list[src]))
+#     print(f"Routes: {routes}")
+#     break
+#     
+# =============================================================================
 
-stack = deque()
+
+def all_routes(src,stacks,curr_route,routes,adj_list):
+    print("Stacks: ", stacks)
+    print("Routes: ", routes)
+    print("Curr Route: ", curr_route)
+    
+    # All Routes Completed 
+    if len(stacks) == 0:
+        return routes
+        
+    # One Node Completed
+    if len(stacks[-1]) == 0:
+        routes.append(curr_route)
+        curr_route = [k]
+        stacks.pop()
+        
+    last_stack = stacks[-1]
+    
+    node = last_stack.pop()
+    curr_route.append(node)
+    
+    # Adding Children of a node
+    if len(adj_list[node]) != 0:
+        stack = deque()
+        
+        for dest in adj_list[node]:
+            stack.append(dest)
+        
+        print("Stack of children: ", stack)
+        stacks.append(stack)
+        
+        return all_routes(src,stacks,curr_route,routes,adj_list)
+    
+    # End of one path
+    
+    routes.append(curr_route)
+    curr_route = curr_route[:-1]
+    
+    return all_routes(src,stacks,curr_route,routes,adj_list)
+    
+    
+        
+    
+    
+    
+    
+
+
+adj_list={1:[2,6,7],2:[3,4],3:[5],4:[],5:[],6:[8],7:[9,10],8:[],9:[],10:[]}
+stacks = []
 routes = []
 
-def push_children(adj_list, stack, routes,curr_route,links_remaining):
-    sleep(0.3)
-    print("Function Called")
-    print("Stack: ", stack)
-    print("Routes: ", routes)
-    print("Curr Route: ",curr_route)
-    print("Links: ",links_remaining)
-    
+for k in adj_list.keys():
+    stack = deque(adj_list[k])
+    stacks.append(stack)
+    routes = all_routes(src=k,stacks=stacks,curr_route=[k],routes=routes,adj_list=adj_list)
+    print(routes)
 
-    
-    
-    if links_remaining == 0:
-        if type(stack[-1]) == str:
-            return routes
-        if len(stack) == 0 or len(stack) == 1:
-            return routes
-        stack.pop()
-        return push_children(adj_list,stack,routes,curr_route[:-1],1)
-    if type(stack[-1])==str:
-        stack.pop()
-    curr_route.append(stack[-1])
- 
 
-    if len(adj_list[curr_route[-1]]) != 0:
-        stack.append(str(len(adj_list[curr_route[-1]])))
-        for next_node in adj_list[curr_route[-1]]:
-            stack.append(next_node)
-            
-        print("Children Added")
-        print("Stack: ", stack)
-        return push_children(adj_list,stack,routes,curr_route,len(adj_list[curr_route[-1]]))
-    
-    print("One Path Complete")
-    
-    routes.append(curr_route)    
-    curr_route = curr_route[:-1]
-    stack.pop()
-    
-    print("Routes: ", routes)
-    print("Stack: ", stack)
-    
-    
-    print("In middle of a branch")
-    return push_children(adj_list,stack,routes,curr_route,links_remaining-1)
-    
 
-for src in adj_list.keys():
-    stack.append(src)
-    routes = push_children(adj_list,stack,routes,[],len(adj_list[src]))
-    print(f"Routes: {routes}")
-    break
-    
+
+
