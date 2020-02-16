01dd5c787b8c2b097bd1e963d02ff2031924e777
susmitvengurlekar <susmit.py@gmail.com>
2020-02-16 22:13:10 +0530

    meeting

--- slot_booking.py
+++ slot_booking.py
@@ -35,6 +35,9 @@ m2 = [["10:00","11:30"],["12:30","14:30"],["14:30","15:00"],["16:00","17:00"]]
 b2 = ["10:00","18:30"]
 d = 30
 
+def convert_to_int(time):
+    return int(time.replace(":",""))
+
 def get_free_time_slots(m,b,d):
     """
     Returns free time slots of duration >= d
@@ -42,19 +45,25 @@ def get_free_time_slots(m,b,d):
     """
     f = []
     # Starting free slot
-    m1s = int(m[0][0].replace(":",""))
-    bs = int(b[0].replace(":",""))
+    m1s = convert_to_int(m[0][0])
+    bs = convert_to_int(b[0])
     
     if (m1s - bs) >= d:
         f.append([b[0],m[0][0]])
         
     # Between free slots
-    
+    n = len(m) # No of meetings
+    for i in range(n-1):
+        mae = convert_to_int(m[i][1])
+        mbs = convert_to_int(m[i+1][0])
         
+        if (mbs - mae >= 30):
+            f.append([m[i][1],m[i+1][0]])
+
 
     # Ending free slot
-    mne = int(m[-1][1].replace(":",""))
-    be = int(b[1].replace(":",""))
+    mne = convert_to_int(m[-1][1])
+    be = convert_to_int(b[1])
     
     if (be - mne) >= d:
         f.append([m[-1][1],b[1]])
@@ -71,6 +80,10 @@ def get_common_free_slots(f1,f2,d):
 
 f1 = get_free_time_slots(m1,b1,d)
 f2 = get_free_time_slots(m2,b2,d)
+
+print("Free time slots:")
+print(f"P1: {f1}")
+print(f"P2: {f2}")
 
 cf = get_common_free_slots(f1,f2,d)
 
