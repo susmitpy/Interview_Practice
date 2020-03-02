3dd4350bfc9f2fece62c38766cd8555b7c92e748
Susmit <susmit.py@gmail.com>
2020-03-03 00:13:46 +0530

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,99 @@
+"""
+Under process.
+
+
+
+"""
+
+import numpy as np
+
+s = np.array([
+       [0, 0, 0, 5, 0, 4, 0, 0, 0],
+       [4, 0, 1, 0, 3, 0, 6, 0, 9],
+       [0, 0, 3, 6, 0, 2, 1, 0, 0],
+       [0, 6, 0,8,4, 7, 0, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 8, 0, 2, 5,9,0, 6, 0],
+       [0, 0, 6, 3, 0, 1, 9, 0, 0],
+       [5, 0, 4, 0, 7, 0, 3, 0, 8],
+       [0, 0, 0, 4, 0, 5, 0, 0, 0]])
+
+def get_box_limits(r,c):
+    r_min, r_max, c_min, c_max = None,None,None,None
+    if r <= 2:
+        r_min = 0
+        r_max = 2
+    elif r <= 5:
+        r_min = 3
+        r_max = 5
+    else:
+        r_min = 6
+        r_max = 8
+    if c <= 2:
+        c_min = 0
+        c_max = 2
+    elif c <= 5:
+        c_min = 3
+        c_max = 5
+    else:
+        c_min = 6
+        c_max = 8
+    return {"ri":r_min,"ra":r_max,"ci":c_min,"ca":c_max}
+
+def get_only_possible(box):
+    """
+        Given a box: 2D array where each element is a set containign possibilities
+        return x,y coord along with the only possible no. at x,y
+    """
+    x=0
+    y=0
+    n=0
+    occurence = {}
+    for i in range(3):
+        for j in range(3):
+            if type(box[i,j]) == set:
+                for k in box[i,j]:
+                    if k in occurence:
+                        occurence[k] = False
+                    else:
+                        occurence[k] = (i,j)
+    for k,v in occurence.items():
+        if type(v) != bool:
+                return (v[0],v[1],k)
+    return (3,3,3)
+
+#sudoku = np.zeros((9,9),dtype=int)
+possibilites = np.full((9,9),set([1,2,3,4,5,6,7,8,9]))
+once_done = False
+for i in range(5):
+    if not once_done:
+        for r in range(9):
+            for c in range(9):
+                if s[r][c] == 0:
+                    row_present = s[r,:]
+                    col_present = s[:,c]
+                    l = get_box_limits(r,c) # Box limits
+                    box = s[l["ri"]:l["ra"]+1,l["ci"]:l["ca"]+1]
+                    box_present = box.flatten()
+                    possibilites[r][c] = possibilites[r][c].difference(row_present.tolist()+col_present.tolist()+box_present.tolist())
+                else:
+                    possibilites[r][c] = s[r][c]
+        once_done = True
+
+    for r in range(0,9,3):
+        for c in range(0,9,3):
+            x,y,n = get_only_possible(possibilites[r:r+3,c:c+3])
+            if x != 3:
+                possibilites[r+x][c+y] = n
+
+    for r in range(9):
+        for c in range(9):
+            if type(possibilites[r,c]) == set:
+                if len(possibilites[r,c]) == 1:
+                    possibilites[r,c] = possibilites[r,c].pop()
+filled = 0
+for r in range(9):
+    for c in range(9):
+        if type(possibilites[r,c]) != set:
+            filled+=1
+print(filled)
